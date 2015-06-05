7d3ebf9339aee1504d88bb8ba62c3b3f35d7959b
erikwuu <erik_hu@hotmail.com>
2015-06-06 00:06:17 +0200

    small changes

--- app/src/main/java/skynet/nl/skynet_whattoeat/Login.java
+++ app/src/main/java/skynet/nl/skynet_whattoeat/Login.java
@@ -5,9 +5,14 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Login extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,26 +20,18 @@ public class Login extends Activity {
         setContentView(R.layout.activity_login);
     }
 
+    public void onClick(View v){
+        EditText username = (EditText)findViewById(R.id.etUsername);
+        String usernameString = username.getText().toString();
+        EditText password = (EditText)findViewById(R.id.etPassword);
+        String passwordString = password.getText().toString();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(usernameString == "admin"){
+            Toast.makeText(getApplicationContext(),"worked",Toast.LENGTH_LONG).show();
         }
-
-        return super.onOptionsItemSelected(item);
+        else{
+            Toast.makeText(getApplicationContext(),"failed",Toast.LENGTH_LONG).show();
+        }
     }
 }
