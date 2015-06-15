2a25ebb0fbc4aa54a53c1e569abbf2b048faac35
erikwuu <erik_hu@hotmail.com>
2015-06-15 14:47:11 +0200

    actionbarrings

--- app/src/main/java/skynet/nl/skynet_whattoeat/ApplicationMenu.java
+++ app/src/main/java/skynet/nl/skynet_whattoeat/ApplicationMenu.java
@@ -3,6 +3,8 @@ package skynet.nl.skynet_whattoeat;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +16,7 @@ public class ApplicationMenu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application_menu);
+        ActionBar bar = getActionBar();
     }
 
     @Override
